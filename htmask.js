f68d7e8585820cae1459a1d38784f8f674241170
Davi Tostes <davisiqueira591@gmail.com>
2025-10-23 13:13:39 -0300

    feat: auto advance

--- htmask.js
+++ htmask.js
@@ -1,46 +1,61 @@
-function mask(value, maskPattern) {
-  if (!value || !maskPattern) return ""
+function mask(value, maskPattern, autoAdvance) {
+  if (!value || !maskPattern) return { maskedValue: "", advanced: false }
 
   const pureValue = value.replace(/[^a-zA-Z0-9]/g, '')
-  let masked = ''
+  let maskedValue = ''
   let valueIdx = 0
+  let advanced = false
 
-  for (let maskChar of maskPattern) {
+  for (let i = 0; i < maskPattern.length; i++) {
     if (valueIdx >= pureValue.length) break;
 
     const inputChar = pureValue[valueIdx]
 
-    if (maskChar !== '0' && maskChar !== 'A') {
-      masked += maskChar
+    if (maskPattern[i] !== '0' && maskPattern[i] !== 'A') {
+      maskedValue += maskPattern[i]
       continue
     }
 
-    if (maskChar === '0' && !/\d/.test(inputChar)) break;
-    if (maskChar === 'A' && !/[a-zA-Z]/.test(inputChar)) break;
+    if (maskPattern[i] === '0' && !/\d/.test(inputChar)) break;
+    if (maskPattern[i] === 'A' && !/[a-zA-Z]/.test(inputChar)) break;
 
-    masked += pureValue[valueIdx++]
+    maskedValue += pureValue[valueIdx++]
+
+    let nextChar = maskPattern[i + 1]
+
+    if (autoAdvance && nextChar && nextChar !== '0' && nextChar !== 'A') {
+      advanced = true
+      maskedValue += nextChar
+      i++
+    }
   }
 
-  return masked
+  return { maskedValue, advanced }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('input[mask]').forEach(input => {
     const maskPattern = input.getAttribute('mask')
+    const autoAdvance = input.getAttribute('mask-auto-advance')
+
     input.maxLength = maskPattern.length
 
     const applyMask = (event) => {
       let cursorPos = event.target.selectionStart
 
-      const maskedValue = mask(event.target.value, maskPattern)
+      const { maskedValue, advanced } = mask(event.target.value, maskPattern, autoAdvance)
       event.target.value = maskedValue
 
       let inCursorPos = maskedValue[cursorPos - 1]
 
-      if (event.data && maskedValue.length >= cursorPos) {
+      if (advanced && event.data && maskPattern[cursorPos] != '0' && maskPattern[cursorPos] != 'A') {
+        cursorPos++
+      }
+
+      if (event.data && maskedValue.length > cursorPos) {
         while (!/\d/.test(inCursorPos) && !/[a-zA-Z]/.test(inCursorPos)) {
-          cursorPos++
           inCursorPos = maskedValue[cursorPos - 1]
+          cursorPos++
         }
       }
 
